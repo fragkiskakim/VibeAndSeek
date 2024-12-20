81d231ef98b450f008008c2a1ccb67748a3b16a1
efrosiniiliadi <164185190+efrosiniiliadi@users.noreply.github.com>
2024-12-20 17:32:27 +0200

    Add files via upload

---
+++ themes/color_palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const Color blueColor = Color(0xFF003366); // Blue
+  static const Color backgroundColor = Color(0xFFF2EBD9); // Background (Beige)
+  static const Color goldColor = Color(0xFFD19348); // Gold
+  static const Color pinkColor = Color(0xFFD88D7F); // Pink
+}
